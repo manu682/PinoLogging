655a19079456f002f702bd95ed133c1b5bc60c30
Manu Mathew <manu682@users.noreply.github.com>
2019-02-17 18:56:31 +0530

    Update index.js
    Added the putput logs in the code

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const auditLogger = logger.child({ tag: 'AUDIT_LOG' });
 
 // Printing a regular log
 logger.info('hello world');
+// {"level":"info","time":1550407076964,"msg":"hello world","v":1}
 
 // Printing personal information with a regular Log
 logger.info({ userid: '1234' }, 'hello world');
+// {"level":"info","time":1550407076964,"msg":"hello world","userid":"1234","v":1}
 
 // Printing AUDIT LOG information with a separate logger
 auditLogger.info('hello child!');
+// {"level":"info","time":1550407076964,"msg":"hello child!","tag":"AUDIT_LOG","v":1}
 
 // Printing AUDIT LOG information with a regular log and personal information
 auditLogger.info({ userid: '1234' }, 'hello child!');
+// {"level":"info","time":1550407076964,"msg":"hello child!","tag":"AUDIT_LOG","userid":"1234","v":1}
